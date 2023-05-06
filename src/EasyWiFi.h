802d5b09e069e638b9ad6caa47fce12284edbc17
Daniel P <daniel.patyk@gmx.de>
2023-05-06 22:17:23 +0200

    Initial commit - Changed formatting and some variable names
    Added my name to the file for modification and formatted the files as well as changed some variable names for more clearness.

--- src/EasyWiFi.h
+++ src/EasyWiFi.h
@@ -1,36 +1,35 @@
- /*
- * EasyWiFi
- * Created by John V. - 2020 V 1.4.1
- * 
- * 
- * 
- * 
- * Released into the public domain on github: https://github.com/javos65/EasyWifi-for-MKR1010
- */
-#ifndef EASYWIFI_H
-#define EASYWIFI_H
+/*
+* EasyWiFi
+* Based on Version 1.4.1 by John V. - 2020
+* Released into the public domain on github: https://github.com/javos65/EasyWifi-for-MKR1010
+* Modified by Daniel Patyk May 2023
+* Version: 1.4.2 https://github/SirPytan/EasyWifi
+* Editor:	http://www.visualmicro.com
+*/
+#ifndef EASYWIFI_h
+#define EASYWIFI_h
 
-#include "Arduino.h"
+#include "arduino.h"
 #include <WiFiNINA.h>
 #include <WiFiUdp.h>
 
-// Define AP Wifi-Client parameters
-#define MAXSSID 10                         // MAX number of SSID's listed after search
-#define SSIDBUFFERSIZE 32                  // SSID name BUFFER size
-#define APCHANNEL  5                       // AP wifi channel
-#define SECRET_SSID "UnKnownWireless"		   // Backup SSID - not required
-#define SECRET_PASS "NoPassword"	         // Backup Pass - not required
-#define CREDENTIALFILE "/fs/credfile"
-#define APNAME "EasyWiFi_AP"
-#define MAXCONNECT 4                       // Max number of wifi logon connects before opening AP
-#define ESCAPECONNECT 15                   // Max number of Total wifi logon retries-connects before escaping/stopping the Wifi start
+// Define AccessPoint(AP) Wifi-Client parameters
+#define MAX_SSID 10                          // MAX number of SSID's listed after search
+#define SSID_BUFFER_SIZE 32                   // SSID name BUFFER size
+#define ACCESS_POINT_CHANNEL  5                        // AP wifi channel
+#define SECRET_SSID "UnKnownWireless"	    // Backup SSID - not required
+#define SECRET_PASS "NoPassword"	        // Backup Pass - not required
+#define CREDENTIAL_FILE "/fs/credfile"
+#define ACCESS_POINT_NAME "EasyWiFi_AP"
+#define MAX_CONNECT 4                        // Max number of wifi logon connects before opening AP
+#define ESCAPE_CONNECT 15                    // Max number of Total wifi logon retries-connects before escaping/stopping the Wifi start
 
 // Define UDP settings for DNS 
 #define UDP_PACKET_SIZE 1024          // UDP packet size time out, preventign too large packet reads
-#define DNSHEADER_SIZE 12             // DNS Header
-#define DNSANSWER_SIZE 16             // DNS Answer = standard set with Packet Compression
-#define DNSMAXREQUESTS 32             // trigger first DNS requests, to redirect to own web-page
-#define UDPPORT  53                   // local port to listen for UDP packets
+#define DNS_HEADER_SIZE 12             // DNS Header
+#define DNS_ANSWER_SIZE 16             // DNS Answer = standard set with Packet Compression
+#define DNS_MAX_REQUESTS 32             // trigger first DNS requests, to redirect to own web-page
+#define UDP_PORT  53                   // local port to listen for UDP packets
 
 // Define RGB values for NINALed
 #define RED 16,0,0
@@ -43,30 +42,28 @@
 
 class EasyWiFi
 {
-  public:
+public:
     EasyWiFi();
     void start();
     byte erase();
-    byte apname(char * name);  
+    byte apname(char* name);
     void seed(int value);
     void led(boolean value);
     void useAP(boolean value);
     void NINAled(char r, char g, char b);
-    
-  private:                      
-  void SimpleDecypher(char * textin, char * textout);
-  void SimpleCypher(char * textin, char * textout);
-  byte Check_Credentials();
-  byte Erase_Credentials();
-  byte Write_Credentials(char * buf1,int size1,char * buf2,int size2);
-  byte Read_Credentials(char * buf1,char * buf2);
-  void APWiFiClientCheck();
-  void APDNSScan();
-  void listNetworks();
-  void APSetup();
-  void printWiFiStatus();
-  
-  
+
+private:
+    void SimpleDecypher(char* textin, char* textout);
+    void SimpleCypher(char* textin, char* textout);
+    byte Check_Credentials();
+    byte Erase_Credentials();
+    byte Write_Credentials(char* buf1, int size1, char* buf2, int size2);
+    byte Read_Credentials(char* buf1, char* buf2);
+    void APWiFiClientCheck();
+    void APDNSScan();
+    void listNetworks();
+    void APSetup();
+    void printWiFiStatus();
 };
 
 #endif
